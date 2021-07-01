3b1f8e0e12d66d31f7fa8599b3ad475877a0330f
cgencdogan <gencdogancaner@gmail.com>
2021-07-01 20:22:26 +0300

    Add project files.

---
+++ UzaktanEgitim.Entities/Concrete/Course.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using Core.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UzaktanEgitim.Entities.Concrete
+{
+    public class Course : IEntity
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public string CoverImgPath { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public virtual ICollection<AppUser> Users { get; set; }
+    }
+}
---
+++ UzaktanEgitim.Entities/Concrete/Lesson.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+
+namespace UzaktanEgitim.Entities.Concrete
+{
+    public class Lesson : IEntity
+    {
+        public int LessonId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int CourseId { get; set; }
+        public int VideoPath { get; set; }
+        public int VideoLength { get; set; }
+        public int LessonOrder { get; set; }
+        public int CreatedBy { get; set; }
+        public int CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public int UpdatedDate { get; set; }
+    }
+}
